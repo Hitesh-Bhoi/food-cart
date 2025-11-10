a872ce9aa03d821ed2c61f6857cb9d593afbeb0f
bhoi hitesh <bhoihitesh1492@gmail.com>
2025-11-11 00:03:41 +0530

    navbar ui changes

--- src/components/topbar/Topbar.tsx
+++ src/components/topbar/Topbar.tsx
@@ -1,13 +1,17 @@
 "use client";
-import { StyledLogoContainer, StyledNavbarContainer, StyledProfileContainer, StyledProfileMenu, StyledProfileOption, StyledTopbarContainer, StyledUserProfile } from "./topbar.styled";
+import { StyledLogoContainer, StyledMenuBar, StyledNavbarContainer, StyledProfileContainer, StyledProfileMenu, StyledProfileOption, StyledTopbarContainer, StyledUserProfile } from "./topbar.styled";
 import logo from "../../../public/logo.svg"
 import Image from "next/image";
 import { CgProfile } from "react-icons/cg";
 import { IoMdLogOut } from "react-icons/io";
 import { useEffect, useState } from "react";
+import { RxCross1 } from "react-icons/rx";
+import { HiMiniBars3 } from "react-icons/hi2";
+
 const Topbar=()=>{
     const [isProfileToggle, setIsProfileToggle] = useState<boolean>(false);
     const [isScrolled, setIsScrolled] = useState<boolean>(false);
+    const [isNavbarShow, setIsNavbarShow] = useState<boolean>(false);
     
     useEffect(()=>{
       const handleScroll=()=>{
@@ -19,36 +23,53 @@ const Topbar=()=>{
     },[])
     return (
       <>
-        <StyledTopbarContainer className={`${isScrolled ? 'scroll' : ''}`}>
+        <StyledTopbarContainer className={`${isScrolled ? "scroll" : ""} ${isNavbarShow ? "active" : ""}`}>
           {/* logo */}
           <StyledLogoContainer>
             <Image src={logo} alt="website logo" />
+            <StyledMenuBar>
+              {isNavbarShow ? (
+                <RxCross1
+                className="cross-line"
+                onClick={() => setIsNavbarShow(false)}
+                />
+              ) : (
+                <HiMiniBars3 className="cross-line" onClick={() => setIsNavbarShow(true)} />
+              )}
+            </StyledMenuBar>
           </StyledLogoContainer>
 
           {/* navbar */}
-          <StyledNavbarContainer>
+          <StyledNavbarContainer className={`${isNavbarShow ? "active" : ""}`}>
             <ul>
               <li>Home</li>
               <li>Browse Menu</li>
               <li>Special Offers</li>
               <li>Restaurants</li>
               <li>Track Orders</li>
+              <li className="show-on-mobile">My Profile</li>
+              <li className="show-on-mobile">Logout</li>
             </ul>
           </StyledNavbarContainer>
 
           {/* profile manu */}
           <StyledProfileContainer>
             <StyledUserProfile>
-              <CgProfile className="profile-icon" onClick={()=>setIsProfileToggle(!isProfileToggle)}/>
-              <StyledProfileMenu className={`${isProfileToggle ? "active" : ""}`}>
-                  <StyledProfileOption>
-                    <CgProfile className="profile-option-icon"/>
-                    <span>My Profile</span>
-                  </StyledProfileOption>
-                  <StyledProfileOption>
-                    <IoMdLogOut className="profile-option-icon"/>
-                    <span>Logout</span>
-                  </StyledProfileOption>
+              <CgProfile
+                className="profile-icon"
+                onClick={() => setIsProfileToggle(!isProfileToggle)}
+              />
+              <StyledProfileMenu
+                className={`${isProfileToggle ? "active" : ""}`}
+              >
+                <StyledProfileOption>
+                  <CgProfile className="profile-option-icon" />
+                  <span>My Profile</span>
+                </StyledProfileOption>
+                <StyledProfileOption>
+                  <IoMdLogOut className="profile-option-icon" />
+                  <span>Logout</span>
+                </StyledProfileOption>
               </StyledProfileMenu>
             </StyledUserProfile>
           </StyledProfileContainer>
--- src/components/topbar/topbar.styled.tsx
+++ src/components/topbar/topbar.styled.tsx
@@ -13,13 +13,56 @@ export const StyledTopbarContainer = styled.div`
   z-index: 999;
   transition: all 0.3s ease;
 
-  &.scroll{
+  &.scroll {
     background-color: white;
-    box-shadow: 0 4px 12px rgba(0,0,0,0.12);
+    box-shadow: 0 4px 12px rgba(0, 0, 0, 0.12);
     height: 70px;
   }
+
+  /* mobile screen */
+  @media (max-width: 1024px) {
+    position: static;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+    padding: 10px;
+    box-shadow: 0 4px 12px rgba(0, 0, 0, 0.12);
+    height: 40px;
+    &.scroll {
+        height: 40px;
+      }
+    &.active {
+      height: 400px;
+      &.scroll {
+        height: 400px;
+      }
+    }
+  }
 `;
-export const StyledLogoContainer = styled.div``;
+
+export const StyledLogoContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  @media (max-width: 1024px) {
+    width: 100%;
+  }
+`;
+
+export const StyledMenuBar = styled.div`
+  display: none;
+
+  @media (max-width: 1024px) {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    .cross-line {
+      font-size: 30px;
+    }
+  }
+`;
 
 export const StyledNavbarContainer = styled.div`
   ul {
@@ -28,31 +71,81 @@ export const StyledNavbarContainer = styled.div`
     align-items: center;
     gap: 20px;
 
-    li{
-        list-style-type: none;
-        padding: 10px 18px;
-        border: none;
-        border-radius: 20px;
-        transition: 0.4s  ease-in-out;
+    li {
+      list-style-type: none;
+      padding: 10px 18px;
+      border: none;
+      border-radius: 20px;
+      word-wrap: nowrap;
+      transition: 0.4s ease-in-out;
     }
 
     li:hover {
-        color: white;
-        background-color: #20b820;
+      color: white;
+      background-color: #20b820;
+    }
+
+    .show-on-mobile {
+      display: none;
     }
   }
+
+  @media (max-width: 1024px) {
+    position: absolute;
+    top: -100px;
+    transform: translateY(-100px);
+    opacity: 0;
+    transition: opacity 0.2s ease, transform 0.2s ease;
+    pointer-events: none;
+
+    &.active {
+      left: 0;
+      right: 0;
+      top: 50px;
+      width: 100%;
+      transform: translateY(10px);
+      opacity: 1;
+      transition: opacity 0.3s ease, transform 0.3s ease;
+      pointer-events: auto;
+
+      ul {
+        flex-direction: column;
+        margin-top: 20px;
+        padding: 0px;
+        gap: 10px;
+
+        .show-on-mobile {
+          display: block;
+        }
+      }
+      li {
+        width: 200px;
+        text-align: center;
+      }
+    }
+  }
+
+  /* @media (max-width: 1024px){
+    li{
+      text-wrap-mode: nowrap;
+    }
+  } */
 `;
 
 export const StyledProfileContainer = styled.div`
   position: relative;
+
+  @media (max-width: 1024px) {
+    display: none;
+  }
 `;
 
 export const StyledUserProfile = styled.div`
-  .profile-icon{
+  .profile-icon {
     font-size: 38px;
     cursor: pointer;
   }
-`
+`;
 
 export const StyledProfileMenu = styled.div`
   position: absolute;
@@ -60,7 +153,7 @@ export const StyledProfileMenu = styled.div`
   right: 0;
   opacity: 0;
   transform: translateY(-10px);
-  transition: opacity 0.30s ease, transform 0.30s ease;
+  transition: opacity 0.3s ease, transform 0.3s ease;
   pointer-events: none;
   width: 140px;
   background-color: #20b820;
@@ -70,7 +163,7 @@ export const StyledProfileMenu = styled.div`
     transform: translateY(0px);
     pointer-events: auto;
   }
-  `;
+`;
 
 export const StyledProfileOption = styled.p`
   padding: 8px 12px;
@@ -82,10 +175,10 @@ export const StyledProfileOption = styled.p`
   border-bottom: 1px solid white;
   color: white;
   cursor: pointer;
-  span{
+  span {
     font-size: 14px;
   }
-  .profile-option-icon{
+  .profile-option-icon {
     font-size: 24px;
   }
 `;
