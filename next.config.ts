2ea99ccf9cc7e2456983e29478e0f884d5bdd88a
bhoi hitesh <bhoihitesh1492@gmail.com>
2026-02-14 06:00:38 +0530

    feat: hostname configuration for cloudinary

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // hostname configuration for the images
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/**",
+      }
+    ]
+  },
   compiler: {
     styledComponents: true,
   }
